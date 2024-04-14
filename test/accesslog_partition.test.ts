a3b07404019e7bd5da35afadb0aade78d3ffea42
nkserveren26 <norinoritoshi@outlook.jp>
2024-04-14 22:00:42 +0900

    Initial commit

---
+++ test/accesslog_partition.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AccesslogPartition from '../lib/accesslog_partition-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/accesslog_partition-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AccesslogPartition.AccesslogPartitionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
